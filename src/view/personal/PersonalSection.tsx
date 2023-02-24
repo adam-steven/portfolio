6e014d4e93ec1f263e2aabfca681b98ea46fc52e
adam-steven <55250489+adam-steven@users.noreply.github.com>
2023-02-24 21:58:41 +0000

    added a_ to personal asset folder

--- src/view/personal/PersonalSection.tsx
+++ src/view/personal/PersonalSection.tsx
@@ -17,7 +17,7 @@ export default function PersonalSection({...props}: PersonalSectionProps) {
               })
             }
         </div>
-        <img className='personal-background pe-none' src={`asset/personal/${props.imagePath}`} alt={props.title} />
+        <img className='personal-background pe-none' src={`asset/a_personal/${props.imagePath}`} alt={props.title} />
     </section>
   )
 }
